74200eb383b9268aa65b6fc45d7be51dcb14de21
florence <perezflores34000@gmail.com>
2021-01-19 07:17:04 +0100

    fin

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -8,14 +8,16 @@ def shout(string_shout)
 end
 
 def repeat(string_repeat,number_repeat)
-#    if number_repeat == nil
+#    if number_repeat.nil?
 #     return "#{string_repeat} " * 2
 #     else
-    return       "#{string_repeat} " * number_repeat
+    return "#{string_repeat} " * number_repeat
+    
 end
 
-
-
+def start_of_word(text,choix_lettre)
+    return text[0..choix_lettre-1]#pour être sur le bon index
+end
 
 
 
